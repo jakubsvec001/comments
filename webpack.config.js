345de093436aefc735b6f5df045f1dd97f9baf80
Jake Wija <jakewija@icloud.com[core]>
2020-01-24 13:10:05 -0800

    BEGIN setting up environment: add .eslintrc, circleci config, webpack with react configurations...

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+const ENTRY = path.resolve(__dirname, 'client', 'src', 'index.jsx');
+const OUTPUT = path.resolve(__dirname, 'client', 'dist');
+const BUNDLE_NAME = 'comments.bundle.js';
+
+module.exports = {
+  mode: 'development',
+  entry: ENTRY,
+  output: {
+    path: OUTPUT,
+    filename: BUNDLE_NAME,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(jsx?)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+};
